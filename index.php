ae97760a2c5b64bbb6c76e801c9cd03f2368c7d9
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2019-09-27 03:25:55 +0800

    Initialize Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ignite Test</title>
+    <link rel="stylesheet" href="public/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/assets/css/style.css">
+</head>
+<body>
+    <a href="/server">Server</a>
+    <a href="/client">Client</a>
+    <script src="public/assets/js/bootstrap.min.js"></script>
+    <script src="public/assets/js/jquery-3.4.1.min.js"></script>
+</body>
+</html>
