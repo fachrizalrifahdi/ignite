19dc26e4118512964d43da5f9df61fa62bb48561
Fachrizal Rifahdi <fachrizalrifahdi@gmail.com>
2019-09-27 03:56:39 +0800

    commit for server

--- client/index.php
+++ client/index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Client Page</h1>
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,8 +9,17 @@
     <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
-    <a href="/server">Server</a>
-    <a href="/client">Client</a>
+    <div id="home">
+        <div class="container h-100">
+            <div class="row h-100 text-center justify-content-center align-items-center">
+                <div class="col-12 custom-navigate">
+                    <h1 class="display-1 text-white">Ignite Test</h1>
+                    <a href="/server" class="btn btn-success btn-lg" role="button" aria-pressed="true">Server</a>
+                    <a href="/client" class="btn btn-danger btn-lg" role="button" aria-pressed="true">Client</a>
+                </div>    
+            </div>    
+        </div>
+    </div>
     <script src="public/assets/js/bootstrap.min.js"></script>
     <script src="public/assets/js/jquery-3.4.1.min.js"></script>
 </body>
